ea1ab8d08b5412e8e7db3f183b441837a34f88f8
Grzegorz <165044876+Gr3Fin@users.noreply.github.com>
2024-04-12 15:03:23 +0200

    Update data.py

--- Quiz game/data.py
+++ Quiz game/data.py
@@ -7,7 +7,7 @@ class QuizData:
 
     def __init__(self):
         parameters = {
-            "amount": 1,
+            "amount": 10,
             "type": "boolean"}
 
         response = requests.get(url="https://opentdb.com/api.php", params=parameters)  # the API endpoint URL
